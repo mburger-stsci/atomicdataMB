dd4782ba1ba21f8ee8134717e5395ee2c4289124
Matthew Burger <mburger@stsci.edu>
2019-04-29 12:38:33 -0400

    uploading to pypi

--- setup.py
+++ setup.py
@@ -16,18 +16,18 @@ conf = ConfigParser()
 conf.read(['setup.cfg'])
 metadata = dict(conf.items('metadata'))
 
-PACKAGENAME = metadata.get('package_name', 'physicsMB')
-DESCRIPTION = metadata.get('description', 'Physics helper functions for nexoclom and general use')
+PACKAGENAME = metadata.get('package_name', 'atomicdataMB')
+DESCRIPTION = metadata.get('description', 'Atomic data functions for nexoclom and general use')
 AUTHOR = metadata.get('author', 'Matthew Burger')
 AUTHOR_EMAIL = metadata.get('author_email', '')
 LICENSE = metadata.get('license', 'unknown')
-URL = metadata.get('url', 'https://github.com/mburger-stsci/physicsMB')
+URL = metadata.get('url', 'https://github.com/mburger-stsci/atomicdataMB')
 __minimum_python_version__ = metadata.get("minimum_python_version", "3.6")
 
 # Enforce Python version check - this is the same check as in __init__.py but
 # this one has to happen before importing ah_bootstrap.
 if sys.version_info < tuple((int(val) for val in __minimum_python_version__.split('.'))):
-    sys.stderr.write("ERROR: physicsMB requires Python {} or later\n".format(__minimum_python_version__))
+    sys.stderr.write("ERROR: atomicdataMB requires Python {} or later\n".format(__minimum_python_version__))
     sys.exit(1)
 
 # Import ah_bootstrap after the python version validation
