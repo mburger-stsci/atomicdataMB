583355e5f51a3a8a77bb7215acbd51a7254efac5
Matthew Burger <mburger@stsci.edu>
2019-05-03 15:48:27 -0400

    Updated documentation

--- atomicdataMB/__init__.py
+++ atomicdataMB/__init__.py
@@ -7,7 +7,7 @@ from .atomicmass import atomicmass
 name = 'atomicdataMB'
 __author__ = 'Matthew Burger'
 __email__ = 'mburger@stsci.edu'
-__version__ = '1.0.7'
+__version__ = '1.0.8'
 
 # Verify the database is setup correctly
 initialize_atomicdata()
